35983763d3bdebacebbf6c26812f7e82aef48bfa
JhonatanAlves4 <jhonatan4alves@gmail.com>
2021-05-24 14:13:35 -0300

    Adicionando a classe Projeto

---
+++ src/main/java/br/com/sistema/model/Projeto.java
@@ -0,0 +1,122 @@
+package br.com.sistema.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.sql.Date;
+
+@Entity(name = "projeto")
+public class Projeto {
+
+    //atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(max = 80)
+    @NotNull
+    private String nome;
+
+    @Size(max = 180)
+    @NotNull
+    private String descricao;
+
+    @Size(max = 50)
+    @NotNull
+    private String status;
+
+    @Size(max = 50)
+    @NotNull
+    private String risco;
+
+    @NotNull
+    private double orcamento;
+
+    @NotNull
+    private Date dataInicial;
+
+    @NotNull
+    private Date dataFinal;
+
+    //getter and setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRisco() {
+        return risco;
+    }
+
+    public void setRisco(String risco) {
+        this.risco = risco;
+    }
+
+    public double getOrcamento() {
+        return orcamento;
+    }
+
+    public void setOrcamento(double orcamento) {
+        this.orcamento = orcamento;
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Projeto{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", status='" + status + '\'' +
+                ", risco='" + risco + '\'' +
+                ", orcamento=" + orcamento +
+                ", dataInicial=" + dataInicial +
+                ", dataFinal=" + dataFinal +
+                '}';
+    }
+}
